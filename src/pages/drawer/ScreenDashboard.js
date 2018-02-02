309b208f73f86dce89d7b3e452c80e8b60a4c5e7
digvijaymachale <digvijay.machale@gmail.com>
2018-02-02 10:44:07 +0530

    Added images for sales dashboard

--- src/pages/drawer/ScreenDashboard.js
+++ src/pages/drawer/ScreenDashboard.js
@@ -1,7 +1,7 @@
 
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import GridView from 'react-native-super-grid';
@@ -30,16 +30,11 @@ class ScreenDashboard extends Component {
 
         // Taken from https://flatuicolors.com/
         const items = [
-            { name: 'TURQUOISE', code: '#1abc9c' }, { name: 'EMERALD', code: '#2ecc71' },
-            { name: 'PETER RIVER', code: '#3498db' }, { name: 'AMETHYST', code: '#9b59b6' },
-            { name: 'WET ASPHALT', code: '#34495e' }, { name: 'GREEN SEA', code: '#16a085' },
-            { name: 'NEPHRITIS', code: '#27ae60' }, { name: 'BELIZE HOLE', code: '#2980b9' },
-            { name: 'WISTERIA', code: '#8e44ad' }, { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-            { name: 'SUN FLOWER', code: '#f1c40f' }, { name: 'CARROT', code: '#e67e22' },
-            { name: 'ALIZARIN', code: '#e74c3c' }, { name: 'CLOUDS', code: '#ecf0f1' },
-            { name: 'CONCRETE', code: '#95a5a6' }, { name: 'ORANGE', code: '#f39c12' },
-            { name: 'PUMPKIN', code: '#d35400' }, { name: 'POMEGRANATE', code: '#c0392b' },
-            { name: 'SILVER', code: '#bdc3c7' }, { name: 'ASBESTOS', code: '#7f8c8d' },
+            { name: 'All Products', code: '#ffffff', imageName: '../../images/healer/padlock.png' }, { name: 'Attendance', code: '#ffffff', imageName: '../../images/healer/padlock.png' },
+            { name: 'Place Order', code: '#ffffff', imageName: '../../images/healer/padlock.png' }, { name: 'My Cart', code: '#ffffff', imageName: '../../images/healer/padlock.png' },
+            { name: 'Customer', code: '#ffffff', imageName: '../../images/healer/padlock.png' }, { name: 'Visit Now', code: '#ffffff', imageName: '../../images/healer/padlock.png' },
+            { name: 'Plan Visit', code: '#ffffff', imageName: '../../images/healer/padlock.png' }, { name: 'Categories', code: '#ffffff', imageName: '../../images/healer/padlock.png' },
+
         ];
 
 
@@ -50,8 +45,11 @@ class ScreenDashboard extends Component {
                 style={styles.gridView}
                 renderItem={item => (
                     <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+                        <View  style={styles.imageContainer} >
+                            <Image source={require('../../images/sales/ic_cart_web.png')} style={styles.itemImage} />
+                        </View>
                         <Text style={styles.itemName}>{item.name}</Text>
-                        <Text style={styles.itemCode}>{item.code}</Text>
+                        {/* <Text style={styles.itemCode}>{item.code}</Text> */}
                     </View>
                 )}
             />
@@ -67,17 +65,21 @@ export default ScreenDashboard;
 const styles = StyleSheet.create({
     gridView: {
         paddingTop: 25,
+        paddingBottom: 25,
         flex: 1,
     },
     itemContainer: {
-        justifyContent: 'flex-end',
+        justifyContent: 'center',
         borderRadius: 5,
+        alignItems: 'center',
         padding: 10,
         height: 150,
     },
     itemName: {
         fontSize: 16,
-        color: '#fff',
+        color: '#000',
+        flex: .2,
+        justifyContent: 'flex-end',
         fontWeight: '600',
     },
     itemCode: {
@@ -85,6 +87,22 @@ const styles = StyleSheet.create({
         fontSize: 12,
         color: '#fff',
     },
+
+    itemImage:
+        {  
+            justifyContent: 'center',
+            height: 50,
+            width: 50,
+
+        },
+
+    imageContainer:
+        {
+            flex: .8,
+            padding: 2,
+            justifyContent: 'center',
+
+        },
 });
 
 
